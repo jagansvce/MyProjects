bfb3f7983bfa916855aae069aea174cdfde16d03
jagansvce <jagansvce@gmail.com>
2015-10-29 20:57:45 -0400

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,104 @@
+package org.boss.internal.cars.model;
+
+public class Car {
+
+	private int id;
+	private String title;
+	private String src;
+	private String year;
+	private String make;
+	private String model;
+	private long price;
+	private long miles;
+	private String mpg;
+	private String bodyStyle;
+	private String engine;
+	private String driveType;
+	private String stockNo;
+	private String vin;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public long getMiles() {
+		return miles;
+	}
+	public void setMiles(long miles) {
+		this.miles = miles;
+	}
+	public String getMpg() {
+		return mpg;
+	}
+	public void setMpg(String mpg) {
+		this.mpg = mpg;
+	}
+	public String getBodyStyle() {
+		return bodyStyle;
+	}
+	public void setBodyStyle(String bodyStyle) {
+		this.bodyStyle = bodyStyle;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public String getDriveType() {
+		return driveType;
+	}
+	public void setDriveType(String driveType) {
+		this.driveType = driveType;
+	}
+	public String getStockNo() {
+		return stockNo;
+	}
+	public void setStockNo(String stockNo) {
+		this.stockNo = stockNo;
+	}
+	public String getVin() {
+		return vin;
+	}
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+}
